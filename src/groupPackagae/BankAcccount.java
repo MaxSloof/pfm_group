d955d489a584ec37336550af780ee708b88fe0b4
MaxSloofAca <100281288+MaxSloofAca@users.noreply.github.com>
2022-03-05 10:54:13 +0100

    Fixing package

--- src/groupPackagae/BankAcccount.java
+++ src/groupPackagae/BankAcccount.java
@@ -1,9 +1,10 @@
+package groupPackagae;
 
-public class BankAccount {
+public class BankAcccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Test");
 	}
 
 }
