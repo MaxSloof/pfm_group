95dfd242b7a0ca586a3423bfe4389cd71150ab4e
srv2999 <rithviksai99@gmail.com>
2022-03-11 19:21:58 +0100

    Few additions

--- src/BankingApp/transaction.java
+++ src/BankingApp/transaction.java
@@ -24,19 +24,46 @@ public class transaction {
 		
 		LocalDate date = LocalDate.now();
 		String fromIban = "-";
-		String toIban; //iban of loggeddd in user
-		System.out.print("Please enter the amount to be deposited: ");
+		String toIban = "NL01PFMB0000000001"; //iban of loggedin userId needs to be added!
+		System.out.print("Please enter the amount to be deposited (+): ");
 		double amount = userInputDouble.nextDouble();
 		
 		transactions1[0] = new transaction(date, fromIban, toIban, amount);
 		writeFile(transactions1);
 		}
 	
-	// Gets an array of student objects and appends it to the file 
+	public static void withdrawFunds() {
+		transaction[] transactions1 = new transaction[1];
+		
+		LocalDate date = LocalDate.now();
+		String fromIban = "NL01PFMB0000000001"; //iban of loggedin userId needs to be added!
+		String toIban = "-";
+		System.out.print("Please enter the amount to be withdrawn (-): ");
+		double amount = userInputDouble.nextDouble();
+		
+		transactions1[0] = new transaction(date, fromIban, toIban, amount);
+		writeFile(transactions1);
+	}
+	
+	public static void transferFunds() {
+		transaction[] transactions1 = new transaction[1];
+		
+		LocalDate date = LocalDate.now();
+		String fromIban = "NL01PFMB0000000001"; //iban of loggedin userId needs to be added!
+		System.out.print("Please enter the iban of recepient: ");
+		String toIban = userInputString.nextLine();
+		System.out.print("Please enter the amount to be withdrawn (-): ");
+		double amount = userInputDouble.nextDouble();
+		
+		transactions1[0] = new transaction(date, fromIban, toIban, amount);
+		writeFile(transactions1);
+	}
+	
+	// Gets an array of transaction objects and appends it to the file 
 	 public static void writeFile(transaction[] transactions1){ 
 		 try{ 
 				PrintWriter myFile = new PrintWriter ( 
-						new BufferedWriter ( new FileWriter ("transactions.txt", true))); 
+						new BufferedWriter ( new FileWriter ("/Users/sairithvik/eclipse-workspace/pfmgroupprj/transactions.txt", true))); 
 				myFile.printf("%s,%s,%s,%f",transactions1[0].date, transactions1[0].fromIban,
 						transactions1[0].toIban, transactions1[0].amount); 
 
@@ -47,13 +74,16 @@ public class transaction {
 			} 
 		}
 	  // Constructors 
-		public transaction(String date, String fromIban, String toIban, double amount) {
-			this.date = date;
+		public transaction(LocalDate date, String fromIban, String toIban, double amount) {
+		/*	this.date = date;
 			this.fromIban = fromIban;
 			this.toIban = toIban;
-			this.amount = amount;
+			this.amount = amount; */
 		}
+
+	//public transaction(LocalDate date2, String fromIban2, String toIban2, double amount2) {
+		// TODO Auto-generated constructor stub
+	//}
 	 } 
 	
-
 
