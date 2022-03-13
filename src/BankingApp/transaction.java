ee44b2521f5a44cd787c686c8dbb6b4be742be50
srv2999 <rithviksai99@gmail.com>
2022-03-13 16:30:08 +0100

    Transaction history, and other additions

--- src/BankingApp/transaction.java
+++ src/BankingApp/transaction.java
@@ -3,7 +3,7 @@ package BankingApp;
 import java.io.*;
 import java.text.DecimalFormat;
 import java.util.*;
-import java.time.LocalDate;
+import java.text.SimpleDateFormat;
 
 public class Transaction {
 
@@ -17,6 +17,8 @@ public class Transaction {
 	
 	double amount;
 	double balance;
+	static int numTra = 0; // Keeps track of number of Transactions
+	
 	
 	static Transaction[] transactions = new Transaction[100];
 
@@ -26,7 +28,9 @@ public class Transaction {
 		Transaction[] transactions1 = new Transaction[1];
 		
 		
-		LocalDate date = LocalDate.now();
+		Date currentDate = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		String date = dateFormat.format(currentDate);
 		String fromIban = "-";
 		String toIban = loggedInIban; //iban of loggedin userId needs to be added!
 		System.out.print("Please enter the amount to be deposited (+): ");
@@ -47,7 +51,9 @@ public class Transaction {
 
 		double balance = BankAccount.returnBalance(loggedInUserID); //logged in user's balance
 		
-		LocalDate date = LocalDate.now();
+		Date currentDate = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		String date = dateFormat.format(currentDate);
 		String fromIban = loggedInIban; //iban of loggedin userId needs to be added!
 		String toIban = "-";
 		System.out.print("Please enter the amount to be withdrawn: ");
@@ -69,30 +75,55 @@ public class Transaction {
 
 	
 	// Sai still needs to fix this
-	public static void transferFunds() {
+	public static void transferFunds(int loggedInUserID, String loggedInIban) {
 		Transaction[] transactions1 = new Transaction[1];
-		double fromBalance = 0;
-		double toBalance = 0;
+		double fromBalance = BankAccount.returnBalance(loggedInUserID);
 		
-		LocalDate date = LocalDate.now();
-		String fromIban = "NL01PFMB0000000001"; //iban of loggedin userId needs to be added!
+		Date currentDate = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		String date = dateFormat.format(currentDate);
+		String fromIban = loggedInIban; //iban of loggedin userId needs to be added!
 		System.out.print("Please enter the iban of recepient: ");
 		String toIban = userInputString.nextLine();
 		System.out.print("Please enter the amount to be transferred: ");
 		double amount = userInputDouble.nextDouble();
 			
 			while (amount > fromBalance) {
-				System.out.println("Withdrawal value exceeded your balance!");
+				System.out.println("Insufficient Funds!");
 				System.out.print("Please enter the amount to be transferred: ");
 				amount = userInputDouble.nextDouble();
 		}
 		
+		double toBalance = BankAccount.returnBalance(toIban);	//??Make method in BankAccount class to return balance based on Iban
 			//local variables that represent new balances of both accounts 
 			double updatedFromBalance = fromBalance - amount;
 			double updatedToBalance = toBalance + amount;
 			
+		BankAccount.overwriteBalance(updatedFromBalance, loggedInUserID);
+		
+		BankAccount.overwriteBalance(updatedToBalance, toIban); //make another method in BankAccount so toIban bank balance is also changed
+			
 		transactions1[0] = new Transaction(date, fromIban, toIban, amount);
 		writeFile(transactions1);
+	}
+	
+	
+	public static void searchTransactions(int loggedInUserID, String loggedInIban) {
+		Transaction[] traArray = readFile();
+		String localIban = loggedInIban;
+		
+		//getting user input for specific dates
+		  System.out.print("Please enter the date of transactions you want to see (YYYY/MM/DD): ");
+		  String localDate = userInputString.nextLine();
+		  
+		  System.out.println("Transaction History: ");
+		  
+		  for(int i= 0; i < numTra; i++) {
+			  if (traArray[i].date.equals(localDate) && (traArray[i].fromIban.equals(localIban) || traArray[i].toIban.equals(localIban))) {
+					System.out.println(traArray[i].date + "," + traArray[i].fromIban + "," + traArray[i].toIban + "," +
+							traArray[i].amount);
+				  	}
+		  }
 	}
 	
 	// Gets an array of transaction objects and appends it to the file 
@@ -100,7 +131,7 @@ public class Transaction {
 		 try{ 
 				PrintWriter myFile = new PrintWriter ( 
 						new BufferedWriter ( new FileWriter ("transactions.txt", true))); 
-				myFile.printf("%s,%s,%s,%f %n",transactions1[0].date, transactions1[0].fromIban,
+				myFile.printf("%s,%s,%s,%.2f %n",transactions1[0].date, transactions1[0].fromIban,
 						transactions1[0].toIban, transactions1[0].amount); 
 
 
@@ -109,17 +140,50 @@ public class Transaction {
 				System.out.print("Wrong! (writing)"); 
 			} 
 		}
+	 
+	 public static Transaction[] readFile() {
+			// TODO Auto-generated method stub
+			Transaction[] tra_local = new Transaction[100]; // 100 here is an upper bound 
+			String date;
+			String fromIban, toIban;
+			double amount;
+			String[] current_line = new String[3]; 
+			numTra = 0;
+			try{ 
+				BufferedReader myFile = new BufferedReader (new FileReader("transactions.txt")); 
+				String input_line; 
+
+				while ((input_line = myFile.readLine()) != null){ 
+					current_line = input_line.split(","); 
+
+					date = current_line[0];
+					fromIban = current_line[1];
+					toIban = current_line[2];
+					amount = Double.parseDouble(current_line[3]);
+
+					tra_local[Transaction.numTra] = new Transaction(date, fromIban, 
+					toIban, amount);
+					Transaction.numTra++;
+				} 
+				myFile.close();
+			} catch(IOException e){ 
+				System.out.print("Wrong! (writing)"); 
+			} 
+		
+		return(tra_local);
+		}
+	 
 	  // Constructors 
-		public Transaction(LocalDate date, String fromIban, String toIban, double amount) {
-		/*	this.date = date;
+		public Transaction(String date, String fromIban, String toIban, double amount) {
+			this.date = date;
 			this.fromIban = fromIban;
 			this.toIban = toIban;
-			this.amount = amount; */
+			this.amount = amount; 
 		}
 
-	//public transaction(LocalDate date2, String fromIban2, String toIban2, double amount2) {
+		public Transaction() {
 		// TODO Auto-generated constructor stub
-	//}
+		}
 	 } 
 	
 
